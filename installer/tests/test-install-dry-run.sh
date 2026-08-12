dbb8f5798649ea194ae086e9f809a1da06218fa9
Cameron Payne <cameron.paynevmware@gmail.com>
2026-08-11 22:33:37 -0500

    installer: isolate pacman keyring and stop retry loops

--- installer/tests/test-install-dry-run.sh
+++ installer/tests/test-install-dry-run.sh
@@ -51,6 +51,10 @@ grep -Fq -- 'intel-ucode' "$TMP/plain.out"
 grep -Fq -- 'amd-ucode' "$TMP/plain.out"
 grep -Fq -- 'sof-firmware' "$TMP/plain.out"
 grep -Fq -- 'file:///var/cache/aurade/repo' "$ROOT/installer/bin/aurade-install"
+grep -Fq -- 'pacstrap -M -G -C' "$ROOT/installer/bin/aurade-install"
+grep -Fq -- 'GPGDir = ${ARCH_GPG_DIR}' "$ROOT/installer/bin/aurade-install"
+grep -Fq -- 'pacman-key --gpgdir "$ARCH_GPG_DIR" --populate archlinux' "$ROOT/installer/bin/aurade-install"
+grep -Fq -- 'package downloads were stopped instead of being retried' "$ROOT/installer/bin/aurade-install"
 acquire_line=$(grep -n -- '--disable-sandbox -Syy' "$TMP/plain.out" | head -1 | cut -d: -f1)
 wipe_line=$(grep -n -- 'wipefs --all --force' "$TMP/plain.out" | head -1 | cut -d: -f1)
 (( acquire_line < wipe_line ))
