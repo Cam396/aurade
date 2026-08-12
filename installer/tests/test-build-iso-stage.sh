01bd613c0cebed65de6d5421870fdd8daf345aa8
Cameron Payne <cameron.paynevmware@gmail.com>
2026-08-12 11:13:21 -0500

    installer: enlarge live overlay for package acquisition

--- installer/tests/test-build-iso-stage.sh
+++ installer/tests/test-build-iso-stage.sh
@@ -36,6 +36,8 @@ actual=$(find "$staged" -maxdepth 1 -type f -name '*.pkg.tar.*' \
 grep -Fxq reflector "$ROOT/installer/archiso/packages.x86_64"
 grep -Fxq archlinux-keyring "$ROOT/installer/archiso/packages.x86_64"
 grep -Fxq DisableDownloadTimeout "$ROOT/installer/archiso/pacman.conf"
+grep -Fq 'cow_spacesize=4G' \
+  "$ROOT/installer/archiso/efiboot/loader/entries/01-aurade-linux.conf"
 [[ -x $TMP/work/profile/airootfs/usr/local/sbin/aurade-refresh-mirrors ]]
 [[ -L $TMP/work/profile/airootfs/etc/systemd/system/multi-user.target.wants/aurade-refresh-mirrors.service ]]
 [[ ! -e $TMP/work/profile/airootfs/etc/systemd/system/multi-user.target.wants/sshd.service ]]
