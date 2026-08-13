1fa2fa750d224995a614dfc0ec99f7b6ad7f893c
Cameron Payne <cameron.paynevmware@gmail.com>
2026-08-13 14:51:30 -0500

    installer: journal cancellation signals

--- installer/tests/test-install-dry-run.sh
+++ installer/tests/test-install-dry-run.sh
@@ -66,6 +66,8 @@ grep -Fq -- 'invalid Arch package signature' "$ROOT/installer/bin/aurade-install
 grep -Fq -- 'cause=keyring_error' "$ROOT/installer/bin/aurade-install"
 grep -Fq -- 'cause=capacity_error' "$ROOT/installer/bin/aurade-install"
 grep -Fq -- 'cause=network_error' "$ROOT/installer/bin/aurade-install"
+grep -Fq -- 'cause=cancelled' "$ROOT/installer/bin/aurade-install"
+grep -Fq -- 'trap '\''handle_cancel 130'\'' INT' "$ROOT/installer/bin/aurade-install"
 grep -Fq -- 'journal_message=${message:0:256}' "$ROOT/installer/bin/aurade-install"
 grep -Fq -- 'without touching the target disk' "$ROOT/installer/bin/aurade-install"
 grep -Fq -- 'private keys excluded' "$TMP/plain.out"
